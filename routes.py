6d39d0450c720dbe215c186735fb490d1ad069df
Matt Mooney <matt@virtualbox>
2013-09-13 00:42:12 -0700

    Added data dir for structuring the program's file hierarchy. Added relevant routes.

--- routes.py
+++ routes.py
@@ -7,14 +7,21 @@ import os
 from bottle import abort, request, route, static_file
 
 # Static file paths
+CLASSES = os.path.join(os.path.dirname(__file__), 'classes')
 CSS = os.path.join(os.path.dirname(__file__), 'css')
 IMG = os.path.join(os.path.dirname(__file__), 'img')
 JS = os.path.join(os.path.dirname(__file__), 'js')
+TEMPLATES = os.path.join(os.path.dirname(__file__), 'templates')
 
 @route('/testing/')
 def automated_testing():
     """ Serves index file """
     return static_file('testing.html', '')
+
+@route('/testing/data/classes/<filename>')
+def css(filename):
+    """ Serves all classes files """
+    return static_file(filename, root=CLASSES)
 
 @route('/testing/css/<filename>')
 def css(filename):
@@ -30,3 +37,8 @@ def img(filename):
 def js(filename):
     """ Serves all JS files """
     return static_file(filename, root=JS)
+
+@route('/testing/data/templates/<filename>')
+def css(filename):
+    """ Serves all template files """
+    return static_file(filename, root=TEMPLATES)
