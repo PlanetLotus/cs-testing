18dce46d51917665214c8c94f60f334edb55ab5d
Matt Mooney <matt@virtualbox>
2013-10-25 19:37:39 -0700

    Increase test complexity.

--- data/classes/CS141/python_guy/main.py
+++ data/classes/CS141/python_guy/main.py
@@ -1,1 +1,5 @@
+import secondary
+
 print 'This is a test!'
+
+secondary.example()
---
+++ data/classes/CS141/python_guy/secondary.py
@@ -0,0 +1,5 @@
+def example():
+    print 'stuff and things'
+    print 'And next, an error will be produced.'
+
+    print kjhgfkjh
