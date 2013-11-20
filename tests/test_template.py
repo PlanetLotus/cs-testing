8f4eaae979d7c90a354369ff71bf6e3e04ceeee2
Matt Mooney <matt@virtualbox>
2013-11-19 19:32:04 -0800

    Finish setup class for template test.

--- tests/test_template.py
+++ tests/test_template.py
@@ -5,13 +5,44 @@ import os, sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import routes
 
+CLASSES = os.path.join(os.path.dirname(__file__), '../data/classes/')
+EXEC = os.path.join(os.path.dirname(__file__), '../data/exec/')
+TEMPLATES = os.path.join(os.path.dirname(__file__), '../data/templates/')
+
 class TestTemplate(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        pass
+        # Create templates
+        template1 = {
+            'template_name': 'unittest_template1',
+            'required_filenames': ['testreq1', 'testreq2'],
+            'key_file': 'output-key',
+            'script_file': 'input-script',
+            'diff_file': 'diff',
+            'instructor_files': ['instructor_files1.txt', 'instructor_files2.txt'],
+            'review_params': [True, True, True]
+        }
+
+        # Create template directories
+        os.makedirs(TEMPLATES + template1['template_name'])
+
+        # Create necessary files
+        key_file1 = open(TEMPLATES + template1['template_name'] + '/output-key', 'w')
+        key_file1.write('test1')
+        key_file1.close()
+        script_file1 = open(TEMPLATES + template1['template_name'] + '/input-script', 'w')
+        script_file1.write('print "test1"')
+        script_file1.close()
+        instructor_file1 = open (TEMPLATES + template1['template_name'] + '/instructor_files1.txt', 'w')
+        instructor_file1.write('instructor file1 test')
+        instructor_file1.close()
+        instructor_file2 = open (TEMPLATES + template1['template_name'] + '/instructor_files2.txt', 'w')
+        instructor_file2.write('instructor file1 test')
+        instructor_file2.close()
 
     @classmethod
     def tearDownClass(cls):
+        # Delete template directories
         pass
 
     def test_add_template(self):
